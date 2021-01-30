373928d874987d1cdf95bb59bdce827bb305f62d
Ubuntu <ubuntu@ip-10-0-1-202.us-east-2.compute.internal>
2021-01-30 16:53:01 +0000

    minio login pass through

--- apiserver.py
+++ apiserver.py
@@ -1,9 +1,14 @@
 import uvicorn
+import requests
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from starlette.responses import FileResponse
 
 app = FastAPI()
+
+minio_service_url = "http://t2-minio-sv.localdev:9000/minio/webrpc"
+minio_user = "foobar"
+minio_password = "hogehoge"
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -15,9 +20,17 @@ async def index():
 async def get_message():
     return "Hello, world from the server!"
 
-@app.get("/test")
-async def test():
-    return "hogehogehoge"
+@app.get("/api/v1/get_minio_token")
+async def get_minio_token():
+    headers = {
+      "Content-Type": "application/json",
+      "User-Agent": "Mozilla/5.0"
+    }
+    data = '{"id":1,"jsonrpc":"2.0","params":{"username":"%s","password":"%s"},"method":"web.Login"}' % (minio_user, minio_password)
+    response = requests.post(minio_service_url, headers=headers, data=data)#, verify=False)
+
+    token = response.json()["result"]["token"]
+    return token
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
